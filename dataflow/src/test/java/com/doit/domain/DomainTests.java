27313f2dfd922ac945d60674fc00c869eeb2967e
Michael Royle <michaelroyle@Michaels-MacBook-Pro.local>
2019-01-31 11:07:34 -0800

    added test to make sure all domain classes are serializable otherwise beam will not be able to encode it

---
+++ dataflow/src/test/java/com/doit/domain/DomainTests.java
@@ -0,0 +1,22 @@
+package com.doit.domain;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Set;
+
+public class DomainTests extends Assert {
+
+    @Test
+    public void allDomainClassesShouldBeSerializable(){
+        Reflections reflections = new Reflections("com.doit.domain", new SubTypesScanner(false));
+        Set<Class<? extends Object>> allClasses = reflections.getSubTypesOf(Object.class);
+        for (Class clazz : allClasses){
+            assertTrue(Arrays.asList(clazz.getInterfaces()).contains(Serializable.class));
+        }
+    }
+}
